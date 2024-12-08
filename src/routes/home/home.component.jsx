c0aef562f42ed6982d02dbf789faae54bdbdac90
divyanshthakurx <divyanshthakurx@gmail.com>
2024-12-09 00:29:10 +0530

    error

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -39,7 +39,7 @@ const toggleMenu = () => setMenuOpen(!menuOpen);
                 </button>
                 </div>
                 <div className={`w-full ${menuOpen ? '' : 'hidden'}`}>
-                  <div className="w-full items-center justify-between md:flex md:w-auto md:order-1 ">
+                  <div className="w-full items-center justify-between md:flex md:w-auto md:order-1  ">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent md:bg-opacity-20 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                       <li>
                       <div className="block py-2 px-3 text-white bg-[#0A433D] rounded md:bg-transparent md:text-[#C21717] md:p-0 md:dark:text-blue-500" aria-current="page">Home</div>
