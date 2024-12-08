447ecc284f1ac72802596bb09cb92d6dd14cf5e3
divyanshthakurx <divyanshthakurx@gmail.com>
2024-12-09 00:08:54 +0530

    fixed navbar not working

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -5,8 +5,13 @@ import { ReactComponent as UcbsLogo } from "../../assets/img/home/ucbs-logo.svg"
 import { ReactComponent as HpuLogo } from "../../assets/img/home/hpu-logo.svg";
 import 'flowbite';
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
+
+const [menuOpen, setMenuOpen] = useState(false);
+const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return(
     <>
 
@@ -26,15 +31,15 @@ const Home = () => {
                 </div>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                   <Link to="/login" className="hidden md:block text-white bg-[#C21717] hover:bg-[#c21717e5] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2 text-center">Log In</Link>
-                  <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden bg-[#C21717] hover:bg-[#c21717e5] focus:outline-none focus:ring-2 focus:ring-[#C21717] dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                  <button onClick={toggleMenu} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden bg-[#C21717] hover:bg-[#c21717e5] focus:outline-none focus:ring-2 focus:ring-[#C21717] dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                   <span className="sr-only">Open main menu</span>
                   <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                   </svg>
                 </button>
                 </div>
-                <div className="w-full hidden" id="navbar-sticky">
-                  <div className="w-full items-center justify-between md:flex md:w-auto md:order-1 ">
+                <div className={`w-full ${menuOpen ? '' : 'hidden'}`}>
+                  <div className="w-full items-center justify-between md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent md:bg-opacity-20 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                       <li>
                       <div className="block py-2 px-3 text-white bg-[#0A433D] rounded md:bg-transparent md:text-[#C21717] md:p-0 md:dark:text-blue-500" aria-current="page">Home</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,6 @@ module.exports = {
   },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
   ],
 }
